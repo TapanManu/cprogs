aa477460bcc5d00745109eeba6ea3d34fe2a7fea
TapanManu <48797926+TapanManu@users.noreply.github.com>
2020-01-27 22:27:05 +0530

    Create n_add.c

---
+++ command_line/n_add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int a,b,sum;
+	int i; //for loop counter
+	
+	if(argc<2)
+	{
+		printf("please use \"prg_name value1 value2 ... \"\n");
+		return -1;
+	}
+	
+	sum=0;
+	for(i=1; i<argc; i++)
+	{
+		printf("arg[%2d]: %d\n",i,atoi(argv[i]));
+		sum += atoi(argv[i]);
+	}
+	
+	printf("SUM of all values: %d\n",sum);
+	
+	return 0;
+}
