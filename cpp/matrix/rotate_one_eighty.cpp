5f17688e3972260dee6854744c5062999cafb6d4
Tapan Manu <48797926+TapanManu@users.noreply.github.com>
2020-11-09 22:50:00 +0530

    rotate 180 degree

--- cpp/matrix/rotate_one_eighty.cpp
+++ cpp/matrix/rotate_one_eighty.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void disp(int matrix[][3],int n){
+void disp(int matrix[][4],int n){
     cout<<endl;
     for(int i=0;i<n;i++){
          for(int j=0;j<n;j++){
@@ -14,23 +14,26 @@ void disp(int matrix[][3],int n){
 }
 
 //180 degree
-void rotate(int matrix[][3],int n){
-     
-     for(int i=0;i<=n/2;i++){
-         for(int j=0;j<=n-i-1;j++){
+void rotate(int matrix[][4],int n){
+     for(int i=0;i<n/2;i++){
+         for(int j=0;j<n;j++){
              int temp = matrix[i][j];
              matrix[i][j] = matrix[n-i-1][n-j-1];
              matrix[n-i-1][n-j-1] = temp; 
          }
      }
+     //if n is odd
+     if(n&1){
+         for(int j=0;j<n/2;j++){
+            int temp = matrix[n/2][j];
+            matrix[n/2][j] = matrix[n/2][n-j-1];
+            matrix[n/2][n-j-1] = temp;
+         }
+     }
      disp(matrix,n);
 }
 int main(){
-    int matrix[][3]={{1,2,3},
-                     {4,5,6},
-                     {7,8,9}};
-    int row = sizeof(matrix[0])/sizeof(matrix);
-    int col = sizeof(matrix[0][0])/sizeof(matrix[0]);
-    rotate(matrix,3);
+    int matrix[][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    rotate(matrix,4);
     return 0;
 }
